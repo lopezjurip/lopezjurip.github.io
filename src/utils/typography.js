4dd36d7e204751503a0882a664eeb0ea9582b54d
Patricio López Juri <lopezjuripatricio@gmail.com>
2018-01-04 16:04:19 -0300

    Updates

--- src/utils/typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from "typography";
-import theme from "typography-theme-lawton";
-
-theme.bodyFontFamily = ["Helvetica Neue", "serif"];
-theme.baseFontSize = "20px";
-const typography = new Typography(theme);
-
-export default typography;
